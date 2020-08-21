43b18ef401b33f76f37aa478c5e424b144e7a4b7
anapy <anabelenfunesc@gmail.com>
2020-08-21 20:32:46 +0200

    trying alias

--- src/main/java/com/example/restservices/Greeting.java
+++ src/main/java/com/example/restservices/Greeting.java
@@ -3,10 +3,13 @@ package com.example.restservices;
 public class Greeting {
     private final long id;
     private final String content;
+    private final long number;
+
 
     public Greeting(long id, String content) {
         this.id = id;
         this.content = content;
+        number = 1;
     }
 
     public long getId() {
